e4e046d4ee35e868baf2b11dfa4b89f83764f5a0
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2021-12-22 13:54:40 +0000

    commit

--- book.js
+++ book.js
@@ -4,8 +4,15 @@ const {
 } = mongoose;
 
 const bookSchema = new Schema({
-  title: String,
-  author: String,
+  title: {
+  type: String,
+  required: true,
+  unique: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
   ISBN: Number,
   yearPublication: Number,
   publishingHouse: String
@@ -18,78 +25,3 @@ const Book = mongoose.model('Book', bookSchema);
 
 
 module.exports = Book
-
-
-//*********** OLD CODE - BEFORE SCHEMA OF MONGODB - NOT REQUIRED!  ONLY FOR REFERENCE *************
-
-// module.exports = class Book {
-//     constructor(_title, _author, _ISBN, _yearPublication, _publishingHouse) {
-//         this._title = _title;
-//         this._author = _author;
-//         this._ISBN = _ISBN;
-//         this._yearPublication = _yearPublication;
-//         this._publishingHouse = _publishingHouse;
-
-//     }
-
-//     get title(){
-//         return this._title;
-
-//     }
-//     setTitle(t){
-//         this._title = t;
-//     }
-
-//     get author(){
-//         return this._author;
-
-//     }
-//     setAuthor(h){
-//         this._author = h;
-//     }
-
-//     get ISBN(){
-//         return this._ISBN;
-
-//     }
-//     setISBN(b){
-//         this._ISBN = b;
-//     }
-
-//     get yearPublication(){
-//         return this._yearPublication;
-
-//     }
-//     setYearPublication(y){
-//         this._yearPublication = y;
-//     }
-
-//     get publishingHouse(){
-//         return this._publishingHouse;
-
-//     }
-//     setPublishingHouse(p){
-//         this._publishingHouse = p;
-//     }
-
-
-// }
-
-//*********** OLD CODE! - NORMAL CONSTRUCTOR TO CONNECT WITH POSTMAN - NOT REQUIRED!  ONLY FOR REFERENCE *************
-
-
-// let bookOne = new Book('The Remains of the Day','Kazuo Ishiguro',0-571-15310-0, 1989, 'Faber and faber');
-
-// console.log(bookOne.author);
-// console.log(bookOne.yearPublication);
-
-// class Notebook{
-//     constructor() {
-//         this.name = 'notebook'
-//     }
-// }
-
-// module.exports = {
-//     Book,
-//     Notebook
-// }
