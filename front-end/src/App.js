63cfeac623ad589318aa8a21b045723004676faf
Nathalie Flores <65398774+nafnu@users.noreply.github.com>
2021-12-22 18:45:13 +0000

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import{ Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from 'react-bootstrap/Navbar';
+
+import Book from './components/Book';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ display: 'block', width: 700, padding: 30 }}>
+            <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+src="https://media.geeksforgeeks.org/wp-content/uploads/20210425000233/test-300x297.png"
+            alt="Sample Brand Logo"
+            width="35"
+            className="align-top d-inline-block"
+            height="35"
+          />Ramdon Book
+        </Navbar.Brand>
+      </Navbar>
+    </div>
+    <Routes>
+             <Route path='/' element={<Book />} />
+      </Routes>
+
     </div>
   );
 }
---
+++ front-end/src/components/Book.js
@@ -0,0 +1,84 @@
+//component to view all dogs
+import React from "react";
+import axios from 'axios';
+
+class Book extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          books: [],
+          isLoaded: true
+        }
+     
+    }
+
+    componentDidMount(){
+
+        axios.get('/book')
+            .then((response) => {
+           
+                this.setState( {
+                    books: response.data,
+                    isLoaded: true
+                })
+                
+            })
+            .catch((error)=> {
+            // handle error 
+            // in case we get an error from the server, e.g. the server is offline
+                this.setState({
+                    isLoaded:false,
+                    error
+                })
+                
+            })
+       
+           
+    }
+
+
+    render(){
+        //assign variables using the state
+        const { isLoaded, books} = this.state;
+        
+        //conditional rendering: https://reactjs.org/docs/conditional-rendering.html
+        //if we are waiting for our server to serve us the data render this part of code
+        // also render this if the SERVER is offline
+        if(!isLoaded){
+            return(
+                <div>The page is loading or the SERVER is down...</div>
+            )
+        //render this part of code if we received the data from the server
+        } else {
+            return(
+                <div>
+                    <table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Breed</th>
+                            <th>Neutred</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {books.map(book => (
+                        <tr key={book._id}>
+                            <td>{book._id}</td>
+                            <td>{book._title}</td>
+                            <td >{book._author}</td>
+                            <td >{book._ISBN}</td>
+                    </tr>
+                    ))}
+                    </tbody> 
+                    </table>
+              </div>  
+            )
+        }
+    }
+}
+    
+
+export default Book;
